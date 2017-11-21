eeea92d1beda69f492441462dabddf808c53953b
chenglei <871379171@qq.com>
2017-11-21 09:50:40 +0800

    新文件

---
+++ src/components/element/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="element">
+
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default{
+    data(){
+      return {
+
+      }
+    }
+  }
+</script>
+<style lang="scss" rel="stylesheet/scss">
+  @import "../../assets/scss/mixin.scss";
+
+  .element {
+
+  }
+</style>
